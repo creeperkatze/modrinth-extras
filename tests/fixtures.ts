d507ec748f5dd74099a8f7af4610c7064e77b481
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2026-04-03 19:04:03 +0200

    Add playwright for tests

---
+++ tests/fixtures.ts
@@ -0,0 +1,32 @@
+import { type BrowserContext, chromium, test as base } from '@playwright/test'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const dirname = path.dirname(fileURLToPath(import.meta.url))
+const pathToExtension = path.join(dirname, '..', '.output', 'chrome-mv3')
+
+export const test = base.extend<{
+	context: BrowserContext
+	extensionId: string
+}>({
+	context: async (_, use) => {
+		const context = await chromium.launchPersistentContext('', {
+			channel: 'chromium',
+			args: [
+				`--disable-extensions-except=${pathToExtension}`,
+				`--load-extension=${pathToExtension}`,
+			],
+		})
+		await use(context)
+		await context.close()
+	},
+	extensionId: async ({ context }, use) => {
+		let [serviceWorker] = context.serviceWorkers()
+		if (!serviceWorker) serviceWorker = await context.waitForEvent('serviceworker')
+
+		const extensionId = serviceWorker.url().split('/')[2]
+		await use(extensionId)
+	},
+})
+
+export const expect = test.expect
