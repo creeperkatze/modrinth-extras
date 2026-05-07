809f2a517ab144a1b8408d3e1d92e6090c274196
Creeperkatze <contact@creeperkatze.dev>
2026-05-07 12:27:08 +0200

    Fix analytics export button

--- src/components/AnalyticsExport.vue
+++ src/components/AnalyticsExport.vue
@@ -1,31 +1,18 @@
 <template>
-	<button
-		v-tooltip="formatMessage(messages['analyticsExport.tooltip'])"
-		class="btn icon-only color-accent-contrast"
-		:disabled="exporting"
-		@click="exportImage"
-	>
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			fill="none"
-			stroke="currentColor"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			stroke-width="2"
-			viewBox="0 0 24 24"
+	<ButtonStyled circular type="outlined">
+		<button
+			v-tooltip="formatMessage(messages['analyticsExport.tooltip'])"
+			:disabled="exporting"
+			@click="exportImage"
 		>
-			<path
-				d="M13.997 4a2 2 0 0 1 1.76 1.05l.486.9A2 2 0 0 0 18.003 7H20a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h1.997a2 2 0 0 0 1.759-1.048l.489-.904A2 2 0 0 1 10.004 4z"
-			/>
-			<circle cx="12" cy="13" r="3" />
-		</svg>
-	</button>
+			<CameraIcon />
+		</button>
+	</ButtonStyled>
 </template>
 
 <script setup lang="ts">
-import { defineMessages, useVIntl } from '@modrinth/ui'
+import { CameraIcon } from '@lucide/vue'
+import { ButtonStyled, defineMessages, useVIntl } from '@modrinth/ui'
 import { ref } from 'vue'
 
 const props = defineProps<{ buttonsContainer: HTMLElement }>()
