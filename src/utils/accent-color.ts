c9993b19070a3b38a8070e5b6b6a1ea8e077138e
Creeperkatze <contact@creeperkatze.dev>
2026-08-06 20:28:46 +0200

    Improve accent color feature

--- src/utils/accent-color.ts
+++ src/utils/accent-color.ts
@@ -11,6 +11,8 @@ export function applyAccentColor(settings: Pick<ExtensionSettings, 'accentColor'
 		root.removeProperty('--color-brand-shadow')
 		root.removeProperty('--color-green')
 		root.removeProperty('--color-green-highlight')
+		root.removeProperty('--color-green-bg')
+		root.removeProperty('--loading-bar-gradient')
 		return
 	}
 
@@ -20,4 +22,9 @@ export function applyAccentColor(settings: Pick<ExtensionSettings, 'accentColor'
 	root.setProperty('--color-brand-shadow', `rgba(${r}, ${g}, ${b}, 0.7)`)
 	root.setProperty('--color-green', settings.accentColor.color)
 	root.setProperty('--color-green-highlight', `rgba(${r}, ${g}, ${b}, 0.25)`)
+	root.setProperty('--color-green-bg', `rgba(${r}, ${g}, ${b}, 0.1)`)
+	root.setProperty(
+		'--loading-bar-gradient',
+		`linear-gradient(to right, ${settings.accentColor.color} 0%, rgba(${r}, ${g}, ${b}, 0.5) 100%)`,
+	)
 }
